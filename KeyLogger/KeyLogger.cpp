c17f957b81f29d57d078af9f6afc011a7dcf2ff3
Diego Meza <diego.meza.ext@geodis.com>
2022-01-27 10:57:25 -0600

    proces monitor added

--- KeyLogger/KeyLogger.cpp
+++ KeyLogger/KeyLogger.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <tchar.h>
 #include <functional>
 #include <atlstr.h>
 #include <locale>
@@ -167,6 +168,45 @@ public: static std::string onWindowChange(std::string activeWindow, const std::f
         return activeWindow;
     }
 };
+
+
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+
+void PrintProcessNameAndID(DWORD processID)
+{
+    TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+
+    // Get a handle to the process.
+
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+        PROCESS_VM_READ,
+        FALSE, processID);
+
+    // Get the process name.
+
+    if (NULL != hProcess)
+    {
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        if (EnumProcessModules(hProcess, &hMod, sizeof(hMod),
+            &cbNeeded))
+        {
+            GetModuleBaseName(hProcess, hMod, szProcessName,
+                sizeof(szProcessName) / sizeof(TCHAR));
+        }
+    }
+
+    // Print the process name and identifier.
+
+    _tprintf(TEXT("%s  (PID: %u)\n"), szProcessName, processID);
+
+    // Release the handle to the process.
+
+    CloseHandle(hProcess);
+}
+
 /// <summary>
 /// 
 /// </summary>
@@ -181,16 +221,42 @@ int main()
     // infinite loop each 50 ms
     Looper::DoWhile(delay, monitor, [&word, &activeWindow]() {
         // word assignation
-        word = KeyLogger::onNewWord(word, [](char c) {}, [](std::string w) {
-            // new word appear!!
-            std::cout << w << std::endl;
-        });
+        //word = KeyLogger::onNewWord(word, [](char c) {}, [](std::string w) {
+        //    // new word appear!!
+        //    std::cout << w << std::endl;
+        //});
         // active window assignation
         activeWindow = Monitor::onWindowChange(activeWindow, [](std::string w) {
             // active window has been changed!!
             std::cout << w << std::endl;
         });
     });
-    // for (const auto& value: words) std::cout << value + char(VK_SPACE);
+    //// for (const auto& value: words) std::cout << value + char(VK_SPACE);
+
+    // Get the list of process identifiers.
+
+    //DWORD aProcesses[1024], cbNeeded, cProcesses;
+    //unsigned int i;
+
+    //if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
+    //{
+    //    return 1;
+    //}
+
+
+    //// Calculate how many process identifiers were returned.
+
+    //cProcesses = cbNeeded / sizeof(DWORD);
+
+    //// Print the name and process identifier for each process.
+
+    //for (i = 0; i < cProcesses; i++)
+    //{
+    //    if (aProcesses[i] != 0)
+    //    {
+    //        PrintProcessNameAndID(aProcesses[i]);
+    //    }
+    //}
+
     return 0;
 }
